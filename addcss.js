aa56f1b8d83450f0cdc6e66a658ad5db7a0a1803
quartertone <492588+quartertone@users.noreply.github.com>
2024-07-13 14:01:33 -0700

    Update addcss.js

--- addcss.js
+++ addcss.js
@@ -4,7 +4,7 @@
 // - id = optional ID for style element
 // - addCSS(css, [id])
 function addCSS(css, id=null) {
-  let styl = document.createElement("style");
+let styl = document.querySelector(`#${id}`) ?? document.createElement("style");
   styl.innerHTML = css;
   if (id) styl.id = id;
   document.head.appendChild(styl);
