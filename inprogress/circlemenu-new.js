3062234717a6544aa488ee43948316a192d3ad3a
quartertone <492588+quartertone@users.noreply.github.com>
2024-07-20 17:20:23 -0700

    Update circlemenu-new.js

--- inprogress/circlemenu-new.js
+++ inprogress/circlemenu-new.js
@@ -10,11 +10,12 @@ by @quartertone
 
 const BUTTONCLASS = "btn";
 
-function floatmenu(styles, config, anchor) {
+function floatmenu({styles={}, config={}, menuitems, anchor}={}) {
   //console.log(anchor,config);
   //	anchor.className = "circlemenuinput";
   var TOGGLEID = anchor.id + "toggle";
-  anchor.numitems = config.data.length - 2;
+  anchor.numitems = menuitems.length - 2;
+
 
 
   styles.btnsize ??= getComputedStyle(anchor).getPropertyValue("--btnsize") ?? getComputedStyle(document.documentElement).getPropertyValue("--btnsize") ?? "3em";
@@ -51,16 +52,14 @@ for (let [name,value] of Object.entries(styles)) {
   var endangle = startangle - config.arc;
   var theta = (startangle - endangle) / (anchor.numitems - 1);
   if (theta * anchor.numitems > 360) theta = (startangle - endangle) / anchor.numitems; //correction for full circle menu
-  var vertoffset = (config.hasOwnProperty("alignbottom") && config.alignbottom == true) ? config.ellipse * Math.sin(toradians(-endangle)) : 0; // vertical offset
+  var vertoffset = config.alignbottom == true ? config.ellipse * Math.sin(toradians(-endangle)) : 0; // vertical offset
   var ellipseXaxis = 1;
 
 
 
 
-// TODO: CLEAN UP and change to addCSS here also
-  var style = document.createElement('style');
-  //style.type = 'text/css';
-   style.innerHTML = `
+
+   let menustyle = `
 	
 	 #${anchor.id}.circlemenu input#${TOGGLEID}:checked ~ .show-menu .btn .menuBtn {opacity: 0;}
 	
@@ -69,18 +68,18 @@ for (let [name,value] of Object.entries(styles)) {
  
   for (i = 0; i < anchor.numitems; i++) {
     let nth = i + 1;
-		let inverse = anchor.numitems - i; // so that cascading menu items flow behind item above it // goes with buttonlist.reverse() below
-    style.innerHTML += `
+		let inverse = anchor.numitems - i; // so that cascading menu items flow behind item above it // goes with menuitems.reverse() below
+    menustyle += `
 		#${anchor.id} .btn:nth-child(${inverse}) {
      top:0em;
 		 ${config.style == "rounded" ? "left:0;" : styles.leftright};
 			transition-delay: ${i * styles.flingdelay}s;}
 			
-	#${anchor.id}.circlemenu input#${TOGGLEID}:checked ~ .show-menu .btn:nth-child(${inverse}) {
+		#${anchor.id}.circlemenu input#${TOGGLEID}:checked ~ .show-menu .btn:nth-child(${inverse}) {
 		`;
 
     if (config.style == "linear") {
-      style.innerHTML += `
+      menustyle += `
 			${styles.leftright};
 			top: calc( (${config.direction} *( var(--lineheight) + (${styles.lineheight} + ${styles.gapsize}) * ${i})));
 			opacity:1;
@@ -93,17 +92,18 @@ for (let [name,value] of Object.entries(styles)) {
         thisangle = toradians(endangle + (theta * i));
       }
 
-      style.innerHTML += `
+      menustyle += `
 			left: calc(${styles.radius} * ${ellipseXaxis * Math.cos(thisangle)});
 			top: calc(${styles.radius} * ${config.ellipse * (-Math.sin(thisangle)) - vertoffset}); opacity:1;}`;
     }
   }
 
-  document.head.appendChild(style);
+  
+	addCSS(menustyle,anchor.id + "_circlemenu");
 
 
 
-  buttonlist = config.data;
+  //menuitems = config.data;
 
   var chkbox = document.createElement("input");
   chkbox.type = "checkbox";
@@ -122,7 +122,7 @@ for (let [name,value] of Object.entries(styles)) {
   togglediv.classList.add(BUTTONCLASS);
 	togglediv.classList.add("togglediv");
   for (i = 0; i < 2; i++) {
-    var btn = buttonlist.shift();
+    var btn = menuitems.shift();
     var trigger = document.createElement("div");
     // use div so Lighthouse does not complain about uncrawlable links
     trigger.id = btn.id;
@@ -137,14 +137,9 @@ for (let [name,value] of Object.entries(styles)) {
     togglediv.insertBefore(trigger, null);
   }
 
-
-console.log(buttonlist);
-
-  for (idx in buttonlist.reverse()) {
-		// for (let idx = buttonlist.length -1; idx >=0; idx--) {
-			console.log(idx);
-    var btn = buttonlist[idx];
-		console.log(btn);
+	for (idx in menuitems.reverse()) {
+		// for (let idx = menuitems.length -1; idx >=0; idx--) {
+    var btn = menuitems[idx];
     if (btn.hidden == true) continue;
     // var trigger = document.createElement(btn.link ? "a" : "div");
     var trigger = document.createElement("a");
@@ -210,8 +205,8 @@ function toradians(degrees) {
 ////////////////////////////////////
 
 function autowidth(anchor) {
-  console.log("--AUTOWIDTH--");
-  if (anchor.config.menuwidth) return;
+  //console.log("--AUTOWIDTH--",anchor.config);
+  if (anchor.styles.menuwidth) return;
   anchor.style.setProperty('--menuwidth', "auto");
   let btns = anchor.getElementsByClassName("btn");
 
@@ -234,49 +229,38 @@ function autowidth(anchor) {
   anchor.style.setProperty('--menuwidth', (0.5 + Math.max(...widths)) + "em");
 	
   //console.log("menufixedwidth", (0.5 + Math.max(...widths)) + "em");
-  // if (anchor.config.banner) {
+  if (anchor.config.banner) {
 
-    // //banner options: number of items to show in banner if too narrow		
+    //banner options: number of items to show in banner if too narrow		
 
+    //console.log(widths, (widths.reduce((a, b) => a + b, 0) + "em"));
 
-    // let style = null;
-    // if (!document.getElementById(anchor.id + "_maxwidth")) {
-      // style = document.createElement('style');
-      // // style.type = 'text/css';
-      // style.id = anchor.id + "_maxwidth";
-      // document.head.appendChild(style);
-    // } else {
-      // style = document.getElementById(anchor.id + "_maxwidth");
-    // }
-    // style.innerHTML = "";
-
-    // //console.log(widths, (widths.reduce((a, b) => a + b, 0) + "em"));
-
-    // // THIS gives wrong sum because of font size scaling. menu font is 75% of baseline
-    // // CRAPPY HACK = divide by 75%
-    // style.innerHTML += "\n@media only screen and (min-width:" + ((1 / 0.85) * widths.reduce((a, b) => a + b, 0) + "em") + ") {"
-      // + "main { padding-top: calc(var(--navheight) + " + anchor.styles.lineheight + ") }"
-      // + "#" + anchor.id + " label.show-menu {display: flex; position: fixed; left: 0; flex-direction: row; justify-content: flex-end; width: 100%; top: var(--navheight); }"
-      // + "#" + anchor.id + " .btn:first-child { display:none; }"
-      // //+ "#" + anchor.id + " .btn:first-child { right:0 !important; }" //CRAPPY HACK
-      // + "#" + anchor.id + " .btn:not(:first-child) { position:relative!important; top: 0 !important; opacity: 1 !important; pointer-events: auto; z-index:1000;}" + "}";
+    // THIS gives wrong sum because of font size scaling. menu font is 75% of baseline
+    // CRAPPY HACK = divide by 75%
+		// navheight could be btnsize
+    let bannerstyle = "\n@media only screen and (min-width:" + ((1 / 0.85) * widths.reduce((a, b) => a + b, 0) + "em") + ") {"
+      + "main { padding-top: calc(var(--btnsize) + " + anchor.styles.lineheight + ") }"
+      + "#" + anchor.id + " label.show-menu {display: flex; position: fixed; left: 0; flex-direction: row; justify-content: flex-end; width: 100%; top: var(--btnsize); }"
+      + "#" + anchor.id + " .btn:first-child { display:none; }"
+      //+ "#" + anchor.id + " .btn:first-child { right:0 !important; }" //CRAPPY HACK
+      + "#" + anchor.id + " .btn:not(:first-child) { position:relative!important; top: 0 !important; opacity: 1 !important; pointer-events: auto; z-index:1000;}" + "}";
 
 
-    // if (anchor.config.banner >= 2) {
-      // style.innerHTML += "\n@media only screen and (min-width:" + ((1 / 0.85) * bannerwidths.reduce((a, b) => a + b, 0) + "em") + ") and (max-width:" + ((1 / 0.85) * widths.reduce((a, b) => a + b, 0) + "em") + ") {"
-        // + "main { padding-top: calc(var(--navheight) + " + anchor.styles.lineheight + ") }"
-        // + "#" + anchor.id + " input[type=\"checkbox\"]:not(:checked) ~ label.show-menu {display: flex; position: fixed; left: 0; flex-direction: row; justify-content: flex-end; width: 100%; top: var(--navheight); }"
+    if (anchor.config.banner >= 2) {
+      bannerstyle += "\n@media only screen and (min-width:" + ((1 / 0.85) * bannerwidths.reduce((a, b) => a + b, 0) + "em") + ") and (max-width:" + ((1 / 0.85) * widths.reduce((a, b) => a + b, 0) + "em") + ") {"
+        + "main { padding-top: calc(var(--navheight) + " + anchor.styles.lineheight + ") }"
+        + "#" + anchor.id + " input[type=\"checkbox\"]:not(:checked) ~ label.show-menu {display: flex; position: fixed; left: 0; flex-direction: row; justify-content: flex-end; width: 100%; top: var(--navheight); }"
 
-        // //CRAPPY HACK for fixed icon
-        // + "#" + anchor.id + " input[type=\"checkbox\"]:not(:checked) ~ label .btn:first-child { position: fixed; top: 0 !important; right:0 !important; }";
-      // for (i = 1; i <= anchor.config.banner + 1; i++) {
-        // style.innerHTML += "#" + anchor.id + " input[type=\"checkbox\"]:not(:checked) ~ label .btn:nth-child(" + i + "), ";
-      // }
-      // //style.innerHTML += "#RANDOMBLAH { position:relative!important; top: 0 !important; opacity: 1 !important; pointer-events: auto; z-index:1000;}";
+        //CRAPPY HACK for fixed icon
+        + "#" + anchor.id + " input[type=\"checkbox\"]:not(:checked) ~ label .btn:first-child { position: fixed; top: 0 !important; right:0 !important; }";
+      for (i = 1; i <= anchor.config.banner + 1; i++) {
+        bannerstyle += "#" + anchor.id + " input[type=\"checkbox\"]:not(:checked) ~ label .btn:nth-child(" + i + "), ";
+      }
+      //style.innerHTML += "#RANDOMBLAH { position:relative!important; top: 0 !important; opacity: 1 !important; pointer-events: auto; z-index:1000;}";
 
-    // }
-
-  // }
+    }
+	addcss(bannerstyle,anchor.id + "_maxwidth");
+  }
 }
 
 
@@ -385,7 +369,7 @@ let styles =  {
 		flingdelay: 0.2, // animation delay in seconds (for staggered opening)
 		menuspeed: 1, // animation speed /// change name to menuspeed
 		btnsize: "3em", // size of buttons
-		menuwidth: "8em", // width of menu items
+		//menuwidth: "16em", // width of menu items, default is auto-calculated
 		imgsize: "3em", // defaults to btnsize, this is the size of the image icons within the linear menu items
 		lineheight: "3em", // defaults to btnsize, line height of the linear menu items
 		linearbg: "#449", // bg color of linear menu items
@@ -399,16 +383,17 @@ var configuration = {
 	  rounded: false, // t/f rounded borders
   direction: 1, // default 1; 1 = clockwise/down, -1 = counterclockwise/up
       // this is experimental and needs some work
-	//banner: 1, //working on partial banner display, where X number of items display in banner.
+	//banner: 3, //working on partial banner display, where X number of items display in banner.
   style: "linear", // rounded or linear
   // arc: {auto calculated}, // angle in degrees, default is calculated according to btnsize and radius
   // startpoint: {auto calculated} // angle in degrees, default is calculated according to arc centered on the zenith
           // zero is set to the left side of the X axis just because that's how I like it.
   ellipse: 1, //change this to make the arc narrow(<1) or tall(>1)
   alignbottom: true, // t/f whether to bring the arc down so the two ends line up with menu opener					
-        	
-  // DATA is required
-  data: [ //first two items comprise the menu opener (open / close)
+};
+
+ // DATA is required
+let data=  [ //first two items comprise the menu opener (open / close)
     {
       "id": "id1",
       "src": "https://upload.wikimedia.org/wikipedia/commons/b/be/Earth.svg",
@@ -443,10 +428,11 @@ var configuration = {
       "alt": "Seventh Stage",
       "link": ""
     },
-  ]
-};
-
-floatmenu(styles,configuration, document.querySelector("#btn"));
+  ];
+	
+	
+// styles:styles,config:configuration,
+floatmenu({styles:styles,config:configuration,menuitems:data,anchor:document.querySelector("#btn")});
 
 
 
@@ -491,12 +477,12 @@ floatmenu(styles,configuration, document.querySelector("#btn"));
 
 
 
-  // buttonlist = config.data;
+  // menuitems = config.data;
 
   // var menulabel = document.getElementById(anchor.id + "_menulabel");
 
-  // for (idx in buttonlist) {
-    // let btn = buttonlist[idx];
+  // for (idx in menuitems) {
+    // let btn = menuitems[idx];
     // let trigger = document.createElement("a");
     // trigger.classList.add(BUTTONCLASS);
     // trigger.id = btn.id;
