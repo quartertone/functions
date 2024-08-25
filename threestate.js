daaf864226409c3fb120ed3ba4d6f4af9964a9c4
quartertone <492588+quartertone@users.noreply.github.com>
2024-08-25 14:36:05 -0700

    Update threestate.js

--- threestate.js
+++ threestate.js
@@ -1,8 +1,11 @@
 // - three-state slider initialization function
 // - assocaited styles in threestate.css
-function threestate(target) {
+function threestate(e) {
   // usage:
   // .onclick = function() {threestate(this)}
+  // or
+  // .onclick = threestate;
+  let target = e.target ?? e;
   console.log(target.dataset.state);
   if (!target.dataset.state) target.dataset.state = 0;
   if (target.dataset.state == "3") return "3";
