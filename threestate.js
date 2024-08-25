43fda65911a5edd9c59acc073d5633a28f41bc77
quartertone <492588+quartertone@users.noreply.github.com>
2024-08-25 14:36:34 -0700

    Update threestate.js

--- threestate.js
+++ threestate.js
@@ -6,16 +6,16 @@ function threestate(e) {
   // or
   // .onclick = threestate;
   let target = e.target ?? e;
-  console.log(target.dataset.state);
+  //console.log(target.dataset.state);
   if (!target.dataset.state) target.dataset.state = 0;
   if (target.dataset.state == "3") return "3";
   target.dataset.state = ++target.dataset.state % 3;
 
-  switch (target.dataset.state) {
-    case "0": console.log("off state"); break;
-    case "1": console.log("mid state"); break;
-    case "2": console.log("end state"); break;
-  }
+  //switch (target.dataset.state) {
+  //  case "0": console.log("off state"); break;
+  //  case "1": console.log("mid state"); break;
+  //  case "2": console.log("end state"); break;
+  //}
   console.log(target.dataset.state);
   return target.dataset.state;
 }
